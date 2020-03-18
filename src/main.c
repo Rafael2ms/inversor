3225b106a88a5ce2d160860b747794466c274477
Rafael2ms <45982581+Rafael2ms@users.noreply.github.com>
2020-03-17 22:27:55 -0300

    mudei alguma coisa que fez o programa parar

--- src/main.c
+++ src/main.c
@@ -1,14 +1,56 @@
-/* Data de submissao:
- * Nome:
- * RA:
+/* Data de submissao:17.03.2020
+ * Nome: Rafael Monteiro Marques
+ * RA:204952
  */
 
 #include <stdio.h>
 const int tam_buffer=100;
+int point = 0;
+
+char frase[100];
+
+void push (char cvar){
+	frase[point] = cvar;
+}
+
+char pop (){
+	char cvar = frase[point]; 
+	return cvar;	
+}
 
 int main() {
-  char buffer[tam_buffer];
-  fgets(buffer, tam_buffer, stdin);
-  printf("%s", buffer);
-  return 0;
+	char buffer[tam_buffer];
+	char c;
+	int var;
+	int posi_palavra = 1;
+
+	fgets(buffer, tam_buffer, stdin);
+
+	while (buffer[point-1] != '\n'){
+	
+		if ((buffer[point] == ' ')||(buffer[point] == '\n')){ 
+			//retira caractere da pilha;
+			var = point+1;
+			while (point >= posi_palavra){
+				
+				point--;
+				c = pop();
+				
+				printf("%c", c);
+			}
+			if (buffer[point] != '\n'){
+				printf(" ");
+			}else{
+				printf('\n');
+			}
+			point = var;
+			posi_palavra = var;
+				
+		} else {
+			//coloca caractere na pilha;
+			push(buffer[point]);
+			point++;	
+		}
+	}
+	return 0;
 }
